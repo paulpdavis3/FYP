41f739139bb7f679689e2686979d8093fd4f925c
paulpdavis3 <paulpdavis3@yahoo.ie>
2019-12-15 13:36:37 +0100

    added code to the repository

---
+++ FinalYearProject/main.py
@@ -0,0 +1,97 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang.builder import Builder
+from firebase import firebase
+from kivy.core.window import Window
+
+firebase = firebase.FirebaseApplication('https://c16324311fyp.firebaseio.com/')
+
+# need to figure out how to get just the userIDs or search for unique identifiers
+# https://www.youtube.com/watch?v=Q3HdZMtBQUw
+# post gives garbage string as parent and then multiple pieces of data go afterwards
+# firebase.post('/users', {'nameOfFirstData': 'actualDataValue', 'nameOfSecondData': 'actualDataValue'})
+firebase.post('/users', {'username': 'testymctestface', 'email': 'test@test.test', 'password': '12345'})
+
+
+class ScreenManagement(ScreenManager):
+    username = "none"
+
+
+class TitlePage(Screen):
+    # Window.clearcolor = (0.5, 0.1, 0.1, 0.1)  # Sets the colour of the background. Tuple is in the format (R, G, B, S) S for saturation.
+    pass
+
+
+class LoginPage(Screen):
+    def checkLogin(self, uname, pword):
+        if self.loginLoop(uname, pword) == -1:
+            print('this username and password do not match anything in the database')
+        else:
+            print('log in successful')
+            self.manager.current = 'main'
+
+    def loginLoop(self, uname, pword):
+        while True:
+            results = firebase.get('/users/', None)
+
+            for index in results:
+                if results[index]['username'] == uname and results[index]['password'] == pword:
+                    return 1
+
+            return -1
+
+
+class RegisterPage(Screen):
+    def checkRegister(self, uname, email, pword):
+        if self.registerLoop(uname, email) == -1:
+            print('data not added to the DB')
+        else:
+            firebase.post('/users', {'username': uname, 'email': email, 'password': pword})
+            print('data added to the db successfully')
+            self.manager.current = 'login'
+
+    def registerLoop(self, uname, email):
+        while True:
+            results = firebase.get('/users/', None)
+
+            for index in results:
+                if results[index]['username'] == uname:
+                    print("this username is already taken")
+                    return -1
+                elif results[index]['email'] == email:
+                    print("this email is already taken")
+                    return -1
+
+            return 1
+
+
+class MainPage(Screen):
+    pass
+
+
+class ProgressPage(Screen):
+    pass
+
+
+class ProfilePage(Screen):
+    pass
+
+
+class ClassroomPage(Screen):
+    pass
+
+
+class PlayPage(Screen):
+    pass
+
+
+kv_file = Builder.load_file('fyp.kv')
+
+
+class FYPApp(App):
+    def builder(self):
+        return kv_file
+
+
+if __name__ == '__main__':
+    FYPApp().run()
--- FinalYearProject/venv/Scripts/Activate.ps1
+++ FinalYearProject/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\User\Desktop\Year4\FinalYearProject\venv"
+$env:VIRTUAL_ENV="C:\Users\User\PycharmProjects\FinalYearProject\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
