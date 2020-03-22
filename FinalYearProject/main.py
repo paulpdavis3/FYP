105b9ca61b48825bd1ac9684a309ae1c8c3da0f5
paulpdavis3 <paulpdavis3@yahoo.ie>
2020-03-22 17:55:18 +0000

    allowed for dynamic locking/unlocking of levels when certain xp is gained

--- FinalYearProject/main.py
+++ FinalYearProject/main.py
@@ -1,15 +1,20 @@
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang.builder import Builder
 from firebase import firebase
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.uix.checkbox import CheckBox
 from kivy.core.window import Window
 from kivy.storage.jsonstore import JsonStore
 from os.path import join
+from kivy.uix.progressbar import ProgressBar
 from kivy.graphics import Line
 from kivy.clock import Clock
+from kivy.core.text import Label as CoreLabel
+from kivy.lang.builder import Builder
+from kivy.graphics import Color, Ellipse, Rectangle
 import algo
 import time
 import random
@@ -146,34 +151,43 @@ class ResultsPage(Screen):
     incorrectAnswers = StringProperty()
     xpEarned = StringProperty()
 
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.progressValue = 0
+
     def on_enter(self, *args):
+        self.progressValue = Clock.create_trigger(self.updateBar)
         self.correctAnswers = str(globalVariables.correctAnswers)
         xpEarned = self.correctAnswers * 10  # * timerMultiplier which will be a multiplier if the user finishes under 30 seconds, if over 1 minute to answer all maybe 0.75 multiplier
 
 
 class LoginPage(Screen):
     # gets the username and password if the person has logged in before
-    try:
-        ScreenManagement.store.get('credentials')['username']
-    except KeyError:
-        username = ""
-    else:
-        username = ScreenManagement.store.get('credentials')['username']
-    try:
-        ScreenManagement.store.get('credentials')['password']
-    except KeyError:
-        password = ""
-    else:
-        password = ScreenManagement.store.get('credentials')['password']
+    username = StringProperty()
+    password = StringProperty()
+
+    def on_pre_enter(self, *args):
+        try:
+            ScreenManagement.store.get('credentials')['username']
+        except KeyError:
+            self.username = ""
+        else:
+            self.username = ScreenManagement.store.get('credentials')['username']
+        try:
+            ScreenManagement.store.get('credentials')['password']
+        except KeyError:
+            self.password = ""
+        else:
+            self.password = ScreenManagement.store.get('credentials')['password']
 
     def checkLogin(self, uname, pword):
         if self.loginLoop(uname, pword) == -1:
             print('this username and password do not match anything in the database')
         else:
-            ScreenManagement.store.put('credentials', username=uname, password=pword,
-                                       email=ScreenManagement.store.get('credentials')['email'],
-                                       teacher=ScreenManagement.store.get('credentials')['teacher'],
-                                       classroom=ScreenManagement.store.get('credentials')['classroom'])
+            # ScreenManagement.store.put('credentials', username=uname, password=pword,
+            #                            email=ScreenManagement.store.get('credentials')['email'],
+            #                            teacher=ScreenManagement.store.get('credentials')['teacher'],
+            #                            classroom=ScreenManagement.store.get('credentials')['classroom'])
             self.manager.current = 'main'
 
     def loginLoop(self, uname, pword):
@@ -188,7 +202,7 @@ class LoginPage(Screen):
 
 
 class RegisterPage(Screen):
-    def checkPassword(self, uname, email, pword):
+    def checkPassword(self, uname, email, pword, isTeacher):
         capital = any(x.isupper() for x in pword)
         lowercase = any(x.islower() for x in pword)
         number = any(x.isnumeric() for x in pword)
@@ -198,19 +212,28 @@ class RegisterPage(Screen):
             print(
                 'Make sure that your password is at least 8 characters long and contains 1 uppercase letter and 1 number')
         else:
-            self.checkRegister(uname, email, pword)
+            self.checkRegister(uname, email, pword, isTeacher)
 
-    def checkRegister(self, uname, email, pword):
+    def checkRegister(self, uname, email, pword, isTeacher):
         if self.registerLoop(uname, email) == -1:
             print('data not added to the DB')
         else:
-            firebase.post('/users',
-                          {'username': uname, 'email': email, 'password': pword, 'teacher': 'no',
-                           'classroom': "no classroom"})
-            ScreenManagement.store.put('credentials', username=uname, password=pword, email=email, teacher="no",
-                                       classroom="no classroom")
-            print('data added to the db successfully')
-            self.manager.current = 'login'
+            if isTeacher:
+                firebase.post('/users',
+                              {'username': uname, 'email': email, 'password': pword, 'teacher': "yes",
+                               'classroom': "no classroom", 'add': 0, 'subtract': 0, 'multiply': 0, 'divide': 0})
+                ScreenManagement.store.put('credentials', username=uname, password=pword, email=email, teacher="yes",
+                                           classroom="no classroom", add=0, subtract=0, multiply=0, divide=0)
+                print('data added to the db successfully')
+                self.manager.current = 'login'
+            else:
+                firebase.post('/users',
+                              {'username': uname, 'email': email, 'password': pword, 'teacher': "no",
+                               'classroom': "no classroom", 'add': 0, 'subtract': 0, 'multiply': 0, 'divide': 0})
+                ScreenManagement.store.put('credentials', username=uname, password=pword, email=email, teacher="no",
+                                           classroom="no classroom", add=0, subtract=0, multiply=0, divide=0)
+                print('data added to the db successfully')
+                self.manager.current = 'login'
 
     def registerLoop(self, uname, email):
         while True:
@@ -266,7 +289,21 @@ class StudentClassroomPage(Screen):
 
 
 class TeacherClassroomPage(Screen):
-    pass
+
+    classroom = "no classroom"
+
+    def on_pre_enter(self, *args):
+        try:
+            ScreenManagement.store.get('credentials')['classroom']
+        except KeyError:
+            self.classroom = "no classroom"
+        else:
+            self.classroom = ScreenManagement.store.get('credentials')['classroom']
+
+        if self.classroom == "no classroom":
+            pass
+        else:
+            pass
 
 
 class PlayPage(Screen):
@@ -274,7 +311,39 @@ class PlayPage(Screen):
 
 
 class AdditionPage(Screen):
-    playerXP = 600
+    playerXP = 0
+    # if something breaks there was a block of code here that said:
+    # addButton1 = ObjectProperty() ...
+
+    def on_pre_enter(self, *args):
+        try:
+            ScreenManagement.store.get('credentials')['add']
+        except KeyError:
+            self.playerXP = 0
+        else:
+            self.playerXP = int(ScreenManagement.store.get('credentials')['add'])
+
+        self.disabledButtons()
+
+    def disabledButtons(self):
+        if self.playerXP < 100:
+            self.ids.addButton2.disabled = True
+        if self.playerXP < 200:
+            self.ids.addButton3.disabled = True
+        if self.playerXP < 300:
+            self.ids.addButton4.disabled = True
+        if self.playerXP < 400:
+            self.ids.addButton5.disabled = True
+        if self.playerXP < 500:
+            self.ids.addButton6.disabled = True
+        if self.playerXP < 600:
+            self.ids.addButton7.disabled = True
+        if self.playerXP < 700:
+            self.ids.addButton8.disabled = True
+        if self.playerXP < 800:
+            self.ids.addButton9.disabled = True
+        if self.playerXP < 900:
+            self.ids.addButton10.disabled = True
 
     def playGame(self, levelNumber):
         # algo.algo("add", levelNumber)
@@ -286,7 +355,37 @@ class AdditionPage(Screen):
 
 
 class SubtractionPage(Screen):
-    playerXP = 600
+    playerXP = 0
+
+    def on_pre_enter(self, *args):
+        try:
+            ScreenManagement.store.get('credentials')['subtract']
+        except KeyError:
+            self.playerXP = 0
+        else:
+            self.playerXP = int(ScreenManagement.store.get('credentials')['subtract'])
+
+        self.disabledButtons()
+
+    def disabledButtons(self):
+        if self.playerXP < 100:
+            self.ids.subtractButton2.disabled = True
+        if self.playerXP < 200:
+            self.ids.subtractButton3.disabled = True
+        if self.playerXP < 300:
+            self.ids.subtractButton4.disabled = True
+        if self.playerXP < 400:
+            self.ids.subtractButton5.disabled = True
+        if self.playerXP < 500:
+            self.ids.subtractButton6.disabled = True
+        if self.playerXP < 600:
+            self.ids.subtractButton7.disabled = True
+        if self.playerXP < 700:
+            self.ids.subtractButton8.disabled = True
+        if self.playerXP < 800:
+            self.ids.subtractButton9.disabled = True
+        if self.playerXP < 900:
+            self.ids.subtractButton10.disabled = True
 
     def playGame(self, levelNumber):
         # algo.algo("subtract", levelNumber)
@@ -297,7 +396,37 @@ class SubtractionPage(Screen):
 
 
 class MultiplicationPage(Screen):
-    playerXP = 600
+    playerXP = 0
+
+    def on_pre_enter(self, *args):
+        try:
+            ScreenManagement.store.get('credentials')['multiply']
+        except KeyError:
+            self.playerXP = 0
+        else:
+            self.playerXP = int(ScreenManagement.store.get('credentials')['multiply'])
+
+        self.disabledButtons()
+
+    def disabledButtons(self):
+        if self.playerXP < 100:
+            self.ids.multiplyButton2.disabled = True
+        if self.playerXP < 200:
+            self.ids.multiplyButton3.disabled = True
+        if self.playerXP < 300:
+            self.ids.multiplyButton4.disabled = True
+        if self.playerXP < 400:
+            self.ids.multiplyButton5.disabled = True
+        if self.playerXP < 500:
+            self.ids.multiplyButton6.disabled = True
+        if self.playerXP < 600:
+            self.ids.multiplyButton7.disabled = True
+        if self.playerXP < 700:
+            self.ids.multiplyButton8.disabled = True
+        if self.playerXP < 800:
+            self.ids.multiplyButton9.disabled = True
+        if self.playerXP < 900:
+            self.ids.multiplyButton10.disabled = True
 
     def playGame(self, levelNumber):
         # algo.algo("multiply", levelNumber)
@@ -308,7 +437,37 @@ class MultiplicationPage(Screen):
 
 
 class DivisionPage(Screen):
-    playerXP = 600
+    playerXP = 0
+
+    def on_pre_enter(self, *args):
+        try:
+            ScreenManagement.store.get('credentials')['divide']
+        except KeyError:
+            self.playerXP = 0
+        else:
+            self.playerXP = int(ScreenManagement.store.get('credentials')['divide'])
+
+        self.disabledButtons()
+
+    def disabledButtons(self):
+        if self.playerXP < 100:
+            self.ids.divideButton2.disabled = True
+        if self.playerXP < 200:
+            self.ids.divideButton3.disabled = True
+        if self.playerXP < 300:
+            self.ids.divideButton4.disabled = True
+        if self.playerXP < 400:
+            self.ids.divideButton5.disabled = True
+        if self.playerXP < 500:
+            self.ids.divideButton6.disabled = True
+        if self.playerXP < 600:
+            self.ids.divideButton7.disabled = True
+        if self.playerXP < 700:
+            self.ids.divideButton8.disabled = True
+        if self.playerXP < 800:
+            self.ids.divideButton9.disabled = True
+        if self.playerXP < 900:
+            self.ids.divideButton10.disabled = True
 
     def playGame(self, levelNumber):
         # algo.algo("divide", levelNumber)
